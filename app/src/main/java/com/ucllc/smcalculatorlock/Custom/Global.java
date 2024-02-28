e73547facf16e3f73d6b69d6289f7950787213d8
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2024-02-28 19:41:18 +0600

    Explorer added

--- app/src/main/java/com/ucllc/smcalculatorlock/Custom/Global.java
+++ app/src/main/java/com/ucllc/smcalculatorlock/Custom/Global.java
@@ -3,18 +3,37 @@ package com.ucllc.smcalculatorlock.Custom;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.ucllc.smcalculatorlock.Pages.Browser;
+import com.ucllc.smcalculatorlock.R;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Objects;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -98,5 +117,63 @@ public class Global {
     }
     @Nullable public DBHandler getDBHandler(){
         return dbHandler;
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    public WebView initBrowser(WebView view, ProgressBar progressBar, ImageView iconView, EditText addressBar, LinearLayout errorView, SwipeRefreshLayout swipe){
+        if(view.getVisibility() == WebView.GONE) {
+            errorView.setVisibility(LinearLayout.GONE);
+            view.setVisibility(WebView.VISIBLE);
+        }
+        String token = Objects.requireNonNull(decrypt(Config.BROWSER_VALIDATOR));
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if(error.getDescription().equals("net::ERR_FAILED")){
+                    view.reload();
+                    return;
+                }
+                view.setVisibility(WebView.GONE);
+                errorView.setVisibility(LinearLayout.VISIBLE);
+                TextView message = errorView.findViewById(R.id.failedMessage);
+                Button tryAgain = errorView.findViewById(R.id.tryAgain);
+                message.setText(error.getDescription());
+                tryAgain.setOnClickListener(v -> {
+                    view.setVisibility(WebView.VISIBLE);
+                    errorView.setVisibility(LinearLayout.GONE);
+                    view.reload();
+                });
+                swipe.setRefreshing(false);
+            }
+        });
+        view.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+                if (newProgress < 100 && progressBar.getVisibility() == ProgressBar.GONE) progressBar.setVisibility(ProgressBar.VISIBLE);
+                if(newProgress == 100){
+                    progressBar.setVisibility(ProgressBar.GONE);
+                    swipe.setRefreshing(false);
+                }
+                addressBar.setText((token.equals(view.getUrl()))?"":view.getUrl());
+                if(token.equals(view.getUrl())){
+                    Browser.homeCall = true;
+                    addressBar.setText("");
+                    iconView.setImageDrawable(ContextCompat.getDrawable(Objects.requireNonNull(context), R.drawable.browser));
+                }
+                else {
+                    Browser.homeCall = false;
+                    addressBar.setText(view.getUrl());
+                }
+            }
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                iconView.setImageBitmap(icon);
+            }
+        });
+        view.loadUrl(Objects.requireNonNull(decrypt(Config.BROWSER_VALIDATOR)));
+        return view;
     }
 }
--- app/src/main/java/com/ucllc/smcalculatorlock/Pages/Browser.java
+++ app/src/main/java/com/ucllc/smcalculatorlock/Pages/Browser.java
@@ -1,10 +1,24 @@
 package com.ucllc.smcalculatorlock.Pages;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ucllc.smcalculatorlock.Custom.Config;
@@ -12,22 +26,80 @@ import com.ucllc.smcalculatorlock.Custom.Global;
 import com.ucllc.smcalculatorlock.R;
 import com.ucllc.smcalculatorlock.databinding.ActivityBrowserBinding;
 
+import java.util.Objects;
+
 public class Browser extends AppCompatActivity {
-    private Global global;
+    public static boolean homeCall = false;
     ActivityBrowserBinding binding;
+    WebView browser;
+    private Global global;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityBrowserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.midnight_black));
-
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.midnight_black_button));
         global = new Global(this, this);
-        if(null != global.decrypt(Config.BROWSER_VALIDATOR)){
+        browser = global.initBrowser(binding.view, binding.progress, binding.urlIcon,
+                binding.urlInput, binding.error, binding.swipe);
+        binding.urlInput.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                String inputUrl = textView.getText().toString();
+                Uri uri = Uri.parse(inputUrl);
+                if (uri.getScheme() == null) {
+                    inputUrl = "https://" + inputUrl;
+                    uri = Uri.parse(inputUrl);
+                }
+                browser.loadUrl(uri.toString());
+                binding.urlInput.clearFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if(homeCall){
+                    finish();
+                    return;
+                }
+                if (browser.canGoBack()) {
+                    browser.goBack();
+                } else {
+                    finish();
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
+        binding.swipe.setOnRefreshListener(() -> browser.reload());
 
-            return;
-        }
-        Toast.makeText(this, "Browser can not start!", Toast.LENGTH_SHORT).show();
-        finish();
+        binding.urlIcon.setOnClickListener(view -> {
+            final Dialog dialog = new Dialog(Browser.this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(true);
+            dialog.setContentView(R.layout.dialogue_incognito);
+            if(null != dialog.getWindow()){
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(ContextCompat.getDrawable(Browser.this, R.drawable.dialogue_background));
+            }
+            dialog.show();
+        });
+        binding.home.setOnClickListener(view -> {
+            browser.clearCache(true);
+            browser.clearHistory();
+            browser.loadUrl(Objects.requireNonNull(global.decrypt(Config.BROWSER_VALIDATOR)));
+            binding.urlInput.clearFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(binding.urlInput.getWindowToken(), 0);
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        browser.clearCache(true);
+        browser.destroy();
     }
 }
